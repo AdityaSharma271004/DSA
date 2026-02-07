ca4dbd9dcc738d91403940c217bcb7259ae39882
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2026-02-07 13:54:35 +0530

    Add files via upload

---
+++ subarray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+// int main()
+// {
+//     int n = 5;
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     int maxSum = INT_MIN;
+    
+//     for (int st = 0; st < n; st++)
+//     { int currSum = 0;
+//         for (int end = st; end < n; end++)
+//         {      
+//             currSum += arr[end];
+//             maxSum = max(currSum,maxSum);
+//         }
+    
+//     }
+//     cout<< "max sum "<<maxSum<<endl;
+//     return 0;
+// }
+
+// kadane's algorithm
+
+
+int main(){
+int n = 5;
+    int arr[5] = {1, 2, 3, 4, 5};
+    int maxSum = INT_MIN;
+    int currSum = 0;
+    for(int i=0; i<n;i++){
+        currSum += arr[i];
+        maxSum = max(currSum, maxSum);
+        if(currSum<0){
+            currSum = 0;
+        }
+        cout<<maxSum<<endl;
+    }
+    return 0;
+
+}
