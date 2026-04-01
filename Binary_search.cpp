5dccbe3d7aedb976c44e54dd10378b161c70d161
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2026-04-01 12:36:31 +0530

    Add files via upload

---
+++ Binary_search.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int> arr, int target){
+    int st = 0 , end = arr.size();
+    while(st <= end){
+        int mid = st + (end-st) /2;
+
+        if(target > arr[mid]){
+            st = mid +1;
+        }
+        else if (target < arr[mid])
+        {
+           end = mid -1;
+        }
+        else{
+            return mid;
+        }
+        
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> arr = {0,-1,3,4,5,9,12,14};
+    int target = 4;
+
+    cout<< binarySearch(arr, target)<<endl;
+}
